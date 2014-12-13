5923ce519b0f465bd8021a8f06ea122297ef950b
Kaiyin Zhong <kindlychung@gmail.com>
2014-12-13 03:04:01 +0100

    first

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "translator",
+    packages = ["translator"],
+    version = "0.0.1",
+    entry_points = {"console_scripts": ['gtranslate = translator.shellscript:main']},
+    install_requires = ["beautifulsoup4>=4.0"],
+    description = "Translate text using google translate",
+    author = "Kaiyin Zhong",
+    author_email = "kindlychung@gmail.com",
+    url = "https://github.com/kindlychung/translator",
+    keywords = ["gmail", "email"]
+    )
+
